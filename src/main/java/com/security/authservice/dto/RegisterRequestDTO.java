90fd73868fbd993e1fa4bf373ac50f1aa5caa28d
ThSFernandes <thsalles99@gmail.com>
2024-10-06 12:10:07 -0300

    Adicionar classes de DTO para login, registro e resposta

---
+++ src/main/java/com/security/authservice/dto/RegisterRequestDTO.java
@@ -0,0 +1,4 @@
+package com.security.authservice.dto;
+
+public record RegisterRequestDTO(String name, String email,  String password) {
+}
---
+++ src/main/java/com/security/authservice/dto/ResponseDTO.java
@@ -0,0 +1,4 @@
+package com.security.authservice.dto;
+
+public record ResponseDTO(String name, String token) {
+}
